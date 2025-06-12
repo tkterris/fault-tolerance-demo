67a98019100406168b340d31058a4a457f044653
Trevor Terris <tkterris@gmail.com>
2025-06-12 08:56:04 -0400

    Add probes, and mp health to backend

---
+++ backend/src/main/java/com/redhat/resiliency/backend/RedisConnectionHealthCheck.java
@@ -0,0 +1,24 @@
+package com.redhat.resiliency.backend;
+
+import java.time.Duration;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@Readiness
+@ApplicationScoped
+public class RedisConnectionHealthCheck implements HealthCheck {
+
+    @Inject
+    IncrementService service;
+
+    @Override
+    public HealthCheckResponse call() {
+    	service.keys().await().atMost(Duration.ofMillis(1000));
+        return HealthCheckResponse.up("Redis connection is healthy!");
+    }
+}
